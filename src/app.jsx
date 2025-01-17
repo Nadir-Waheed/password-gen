2b1991cb913f48f6be46777b6bf8e8595ed8a3fa
Nadir Waheed <nadirwaheed@gmail.com>
2025-01-16 23:02:00 -0500

    userForm created

--- src/app.jsx
+++ src/app.jsx
@@ -19,7 +19,24 @@ function App(){
                     <label htmlFor="length">Length</label>
                     <input type="number" id="Length"min={6} max={25}/>
                    </div>
+                   <div className="field">
+                    <label htmlFor="Uppercase">Uppercase</label>
+                    <input type="checkbox"id = "Uppercase" />
+                   </div>
+                   <div className="field">
+                    <label htmlFor="Lowercase">Lowercase</label>
+                    <input type="checkbox"id = "Lowercase" />
+                   </div>
+                   <div className="field">
+                    <label htmlFor="Number">Number</label>
+                    <input type="checkbox"id = "Number" />
+                   </div>
+                   <div className="field">
+                    <label htmlFor="Symbol">Symbol</label>
+                    <input type="checkbox"id = "Symbol" />
+                   </div>
                 </div>
+                <button type="submit">Generate password</button>
             </form>
             </div>  
         </section>
---
+++ src/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+export function useForm(initialValues){
+    const[values,setValues]=userState(initialValues)
+    return[
+        values,
+        (e)=>{
+            setValues({
+                ...values,
+                [e.target.name]:e.target.type ===
+                "checkbox" ? e.target.checked : e.target.value,
+            })
+
+
+        }
+    ]
+
+
+
+
+}
