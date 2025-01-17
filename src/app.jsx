df2927e4841edd20dd302a7a4291c1c6ae633cac
Nadir Waheed <nadirwaheed@gmail.com>
2025-01-16 20:09:07 -0500

    updated app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { FaClipboard } from 'react-icons/fa';
 
 function App(){
-    return<div>App</div>;
+    return(
+        <section>
+          <div className="container">
+            <form id="pg-form">
+                <div className="result">
+                    <input type="text" id="result"
+                    placeholder="Minimum 6 characters"readOnly/>
+                    <div className="clipboard">
+
+                    </div>
+                </div>
+            </form>
+            </div>  
+        </section>
+    )
 }
 export default App;
