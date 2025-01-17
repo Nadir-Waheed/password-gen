754488fa02fbfa4d335252bddeaa50bc139a4bd9
Nadir Waheed <nadirwaheed@gmail.com>
2025-01-16 23:36:44 -0500

    modified app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -12,11 +12,7 @@ function App(){
 
 
     });
-
-
-
-
-
+    
     return(
         <section>
           <div className="container">
@@ -32,23 +28,33 @@ function App(){
                 <div>
                    <div className="field">
                     <label htmlFor="length">Length</label>
-                    <input type="number" id="Length"min={6} max={25}/>
+                    <input type="number" id="Length"min={6} max={25}
+                    name="length" value = {values.length} onCHange={setValues}/>
                    </div>
                    <div className="field">
                     <label htmlFor="Uppercase">Uppercase</label>
-                    <input type="checkbox"id = "Uppercase" />
+                    <input type="checkbox"id = "Uppercase"
+                    name = "uppercase"
+                    checked = {values.uppercase}
+                    onChange = {setValues}
+                    />
                    </div>
                    <div className="field">
                     <label htmlFor="Lowercase">Lowercase</label>
-                    <input type="checkbox"id = "Lowercase" />
+                    <input type="checkbox"id = "Lowercase"
+                    name= "lowercase" checked = {values.lowercase} onChange = {setValues}              
+                    />
                    </div>
                    <div className="field">
                     <label htmlFor="Number">Number</label>
-                    <input type="checkbox"id = "Number" />
+                    <input type="checkbox"id = "Number"
+                    name = "number" checked={values.number} onChange={setValues}
+                    />
                    </div>
                    <div className="field">
                     <label htmlFor="Symbol">Symbol</label>
-                    <input type="checkbox"id = "Symbol" />
+                    <input type="checkbox"id = "Symbol"
+                    name = "symbol" checked={values.symbol} onChange={setValues} />
                    </div>
                 </div>
                 <button type="submit">Generate password</button>
